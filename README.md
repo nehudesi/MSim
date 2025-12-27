cc6fd1e3ed426fa184b9ee896bf930e4cd7f32c8
Chintan Patel <chintanlike@gmail.com>
2025-12-26 22:15:11 -0800

    Switch to yfinance to load data
    Signed-off-by: Chintan Patel <chintanlike@gmail.com>

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ MSim
 
 Market Simulator
 
+sudo apt update
+sudo apt install python3-pip
 python3 -m pip install numpy pandas matplotlib
 
+python3 -m pip install numpy pandas matplotlib
+
+/home/cmpatel/msim/MSim/.venv/bin/python main.py
--- module/yahooDataPull.py
+++ module/yahooDataPull.py
@@ -1,19 +1,13 @@
 '''
 Version: MRT v3.0
 Type: module
-Location: C:\MRT3.0\module
 Author: Chintan Patel
 Email: chintanlike@gmail.com
 
 Pulling Yahoo CSV Data
 '''
 
-try:
-    import urllib.request as urllib2
-    import urllib.parse as urllib
-except Exception:
-    import urllib2
-    import urllib
+import yfinance as yf
 import datetime
 import os
 from module import basic
@@ -36,39 +30,21 @@ def get_data(data_path,ls_symbols):
         if symbol[0] == '$':
             symbol = '^' + symbol[1:]
 
-        symbol_data=list()
-        # print("Getting {0}".format(symbol)
+        # print("Getting {0}".format(symbol))
         
         try:
-            params= urllib.urlencode ({'a':0, 'b':1, 'c':2010, 'd':_now.month, 'e':_now.day, 'f':_now.year, 's': symbol})
-            url = "http://ichart.finance.yahoo.com/table.csv?%s" % params
-            url_get= urllib2.urlopen(url)
-            
-            header= url_get.readline()
-            symbol_data.append (url_get.readline())
-            while (len(symbol_data[-1]) > 0):
-                symbol_data.append(url_get.readline())
-            
-            symbol_data.pop(-1) #The last element is going to be the string of length zero. We don't want to write that to file.
-            #now writing data to file
-            os.chdir(data_path)
-            f = open (symbol_name + ".csv", 'w')
-            
-            #Writing the header
-            f.write (header)
-            
-            while (len(symbol_data) > 0):
-                f.write (symbol_data.pop(0))
-             
-            f.close();    
-
+            start_date = '2010-01-01'
+            end_date = _now.strftime('%Y-%m-%d')
+            df = yf.download(symbol, start=start_date, end=end_date)
+            if df.empty:
+                miss_ctr += 1
+                print("<<<(w) Unable to fetch data for stock: {0}".format(symbol_name))
+            else:
+                df.to_csv(os.path.join(data_path, symbol_name + '.csv'))
         
-        except urllib2.HTTPError:
+        except Exception as e:
             miss_ctr += 1
-            print("<<<(w) Unable to fetch data for stock: {0}".format(symbol_name))
-        except urllib2.URLError:
-            miss_ctr += 1
-            print("<<<(w) URL Error for stock: {0}".format(symbol_name))
+            print("<<<(w) Error fetching data for stock: {0} - {1}".format(symbol_name, str(e)))
             
     print("All done. Got {0} stocks. Could not get {1}".format(len(ls_symbols) - miss_ctr, miss_ctr))
 
